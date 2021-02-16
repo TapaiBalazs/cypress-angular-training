72734032f981d5349e6fdb19e0264dbbf97ac464
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-02-16 14:44:33 +0100

    Set up initial pizza CRUD

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put, Query, ValidationPipe } from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { PizzaDto } from './data-transfer-objects/pizza.dto';
 import { Pizza } from './interfaces/pizza.interfaces';
+
+/**
+ Since this is only a mock back-end, I'm inclined to do everything in one controller.
+ */
 
 @Controller()
 export class AppController {
@@ -10,5 +15,21 @@ export class AppController {
   @Get('pizza/list')
   async getPizzas(): Promise<Pizza[]> {
     return this.appService.getPizzas();
+  }
+
+  // TODO: make these authenticated endpoints
+  @Get('pizza/:id')
+  async getPizza(@Param('id', ParseIntPipe) id: number): Promise<Pizza> {
+    return this.appService.getPizza(id)
+  }
+
+  @Post('pizza')
+  async createPizza(@Body(ValidationPipe) pizzaDto: PizzaDto): Promise<number> {
+    return this.appService.createPizza(pizzaDto);
+  }
+
+  @Put('pizza')
+  async updatePizza(@Body(ValidationPipe) pizzaDto: PizzaDto): Promise<number> {
+    return this.appService.updatePizza(pizzaDto);
   }
 }
--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PizzaDto } from './data-transfer-objects/pizza.dto';
 import { Pizza } from './interfaces/pizza.interfaces';
 
 const PIZZAS = [
   {
-    id: 0,
+    id: 1,
     name: 'Margherita',
     price: 1200,
     imageUrl: 'todo',
@@ -43,5 +44,28 @@ const PIZZAS = [
 export class AppService {
   async getPizzas(): Promise<Pizza[]> {
     return PIZZAS;
+  }
+
+  async getPizza(id: number): Promise<Pizza> {
+    return PIZZAS.find(p => p.id === id);
+  }
+
+  async createPizza(pizza: PizzaDto): Promise<number> {
+    const id = Math.floor(Math.random() * 100000);
+    const newPizza = {
+      id,
+      ...pizza
+    }
+    PIZZAS.push(newPizza);
+    return id;
+  }
+
+  async updatePizza(pizza: PizzaDto): Promise<number> {
+    const existing = await this.getPizza(pizza.id);
+    existing.name = pizza.name
+    existing.price = pizza.price
+    existing.description = pizza.description
+    existing.imageUrl = pizza.imageUrl
+    return existing.id;
   }
 }
