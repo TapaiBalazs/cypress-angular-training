49e68a63229ec697ee7f714844a938eecf3195d9
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-02-22 16:47:20 +0100

    Orders page tests

---
+++ apps/backoffice-functional/src/integration/orders.spec.ts
@@ -0,0 +1,33 @@
+describe(`Orders page`, () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'api/orders', { fixture: 'orders.json' });
+
+    cy.window().then((window) => {
+      window.sessionStorage.setItem('AUTH_TOKEN', `${new Date().getTime()}`);
+    });
+
+    cy.visit('/admin/orders');
+  });
+
+  it(`displays the orders`, () => {
+    cy.get(`[data-test-id="order item 1"]`)
+      .should('be.visible')
+      .find('summary')
+      .should('contain', '$26.90, CARD - Address: Gotham, Crime Alley 32.');
+
+    cy.get(`[data-test-id="order item 2"]`)
+      .should('be.visible')
+      .find('summary')
+      .should('contain', '$13.45, CASH - Address: Gotham, Arkham street 92');
+  });
+
+  it(`the orders contain the ordered pizzas`, () => {
+    cy.get(`[data-test-id="order item 1"]`)
+      .should('be.visible')
+      .find('summary')
+      .click();
+    cy.get(`[data-test-id="order item 1"]`)
+      .find(`[data-test-id="Diavola"]`)
+      .should('be.visible');
+  });
+});
