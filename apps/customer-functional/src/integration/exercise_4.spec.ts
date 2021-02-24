64d9af01764f6d86b4bdd051db398715fb3b0a1b
Balázs Tápai <balazs.tapai1990@gmail.com>
2021-02-24 16:39:14 +0100

    Ex4 step 8

--- apps/customer-functional/src/integration/exercise_4.spec.ts
+++ apps/customer-functional/src/integration/exercise_4.spec.ts
@@ -81,7 +81,7 @@ describe(`Exercise 4 - The Cart page`, () => {
     beforeEach(() => {
       cy.visit('/cart', {
         onBeforeLoad: (window: AUTWindow) => {
-          window.localStorage.setItem('cart', JSON.stringify(MOCK_CART_CONTENT))
+          window.localStorage.setItem('cart', JSON.stringify(MOCK_CART_CONTENT));
         }
       });
     });
@@ -116,7 +116,7 @@ describe(`Exercise 4 - The Cart page`, () => {
 
     describe(`placing an order`, () => {
 
-      it(`a successful order navigates to the /success page and clears the cart contents`, () => {
+      beforeEach(() => {
         cy.get('#city')
           .should('be.visible')
           .and('not.disabled')
@@ -131,7 +131,9 @@ describe(`Exercise 4 - The Cart page`, () => {
           .should('be.visible')
           .and('not.disabled')
           .select('CARD');
+      });
 
+      it(`a successful order navigates to the /success page and clears the cart contents`, () => {
         cy.intercept('POST', '/api/order', { body: 1 }).as('orderRequest');
 
         cy.get(`[data-test-id="place order"]`)
@@ -154,6 +156,32 @@ describe(`Exercise 4 - The Cart page`, () => {
           .should('be.visible')
           .and('have.css', 'opacity', '0.7')
           .and('contain', '$0');
+      });
+
+      it(`when an error occurs, user should be redirected to the error page with the cart contents kept`, () => {
+        cy.intercept('POST', '/api/order', { forceNetworkError: true }).as('orderRequest');
+
+        cy.get(`[data-test-id="place order"]`)
+          .should('be.visible')
+          .and('not.be.disabled')
+          .click();
+
+        cy.wait('@orderRequest')
+          .its('request.body').should('deep.include', {
+          address: 'Gotham, Crime Alley 32.',
+          paymentType: 'CARD'
+        });
+
+        cy.url().should('contain', '/error');
+
+        cy.get(`[data-test-id="error message"]`)
+          .should('be.visible')
+          .and('contain', 'Sorry, an unexpected error occurred. Please try again later.');
+
+        cy.get(`[data-test-id="cart button"]`)
+          .should('be.visible')
+          .and('have.css', 'opacity', '1')
+          .and('contain', '$26.90');
       });
     });
 
